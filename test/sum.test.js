86c2044cdc42973d2f6ed8c95ac3f9a5c1a4e149
Yura Abharian <yuraabharian@gmail.com>
2022-06-09 21:03:32 +0300

    add a new test

--- test/sum.test.js
+++ test/sum.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 2 + 2 to equal 4', () => {
   expect(sum(2, 2)).toBe(4)
 })
+
+test('adds 1 + 1 to equal 2', () => {
+  expect(sum(1, 1)).toBe(2)
+})
