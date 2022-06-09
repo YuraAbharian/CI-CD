aaa7f5fafbc8aefb3f4f82f8875eb188b0b72bf8
Yura Abharian <yuraabharian@gmail.com>
2022-06-09 21:51:50 +0300

    set up netlify support

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express')
+const serverless = require("serverless-http")
+
 const sum = require('./helper/sum')
 
 const app = express()
+const router = express.Router()
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.get('/sum', (req, res) => {
-  const { a = 0, b = 0 } = req.query
+router.get('/sum', (req, res) => {
+  const { a, b } = req.query
   res.send(`RESULT: ${sum(a, b)}`)
 })
 
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log('TEST APP IS APP ON PORT:', PORT)
-})
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app
+module.exports.handler = serverless(app)
